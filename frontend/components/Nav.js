a11bf7859efc2a7ff564e75d393f1b696462c916
maccorzo <marko@hankkila.me>
2019-01-13 21:07:34 +0200

    After video 26
    Currently logged in User with middleware and render props

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,7 +1,17 @@
 import Link from 'next/link';
 import NavStyled from './styles/NavStyles';
+import User from './User';
 const Nav = () => (
   <NavStyled>
+    <User>
+      {({ data: { me } }) => {
+        console.log(me);
+        if (me) {
+          return <p>{me.name}</p>;
+        }
+        return null;
+      }}
+    </User>
     <Link href="/items">
       <a>Shop</a>
     </Link>
